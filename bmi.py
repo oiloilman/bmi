0732479848cea0f12aff4addd6cb08420f86a647
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-09 20:23:52 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
 import streamlit as st
     
-w = st.number_(input('請輸入體重(KG)？'))
-h = st.number_(input('請輸入身高(M)？'))
+w = st.number_input('請輸入體重(KG)？'))
+h = st.number_input('請輸入身高(M)？'))
 confirm_input = st.button('請輸入確認')
 
 if confirm_input:
